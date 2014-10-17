beb63163156da08dc9b4d981b8f568a91fbfd32e
bgrayd <bgrayd@gmail.com>
2014-10-17 15:31:50 -0500

    Create MPLABX project
    Due to build issues and the different configurations make the different
    versions easier

--- NeverWanderBand.c
+++ NeverWanderBand.c
@@ -5,9 +5,9 @@
 //This is for testing different modules and will call the functions the way the parent or child will call them
 int main(void) {
   configClock();
-  configPinsForLowPower();
   configHeartbeat();
-  configDefaultUART(DEFAULT_BAUDRATE);
+  configUART1(DEFAULT_BAUDRATE);
+  configUART2(DEFAULT_BAUDRATE);
   printResetCause();       //print statement about what caused reset
   outString(HELLO_MSG);
 }
