b7ed3cd51dcc52fb69d5e1198056df50a8381755
Ravi <rs1164@msstate.edu>
2014-11-06 16:18:30 -0600

    Fixed Build Errors
    Fixed Silly Errors

---
+++ NeverWanderBandMain.X/build/default/production/_ext/1270305794/pic24_serial.o.d
@@ -0,0 +1,8 @@
+build/default/production/_ext/1270305794/pic24_serial.o:  \
+ ../../../../../../ece3724/pic24_code_examples/lib/common/pic24_serial.c  \
+../../../../../../ece3724/pic24_code_examples/lib/include/pic24_serial.h  \
+../../../../../../ece3724/pic24_code_examples/lib/include/pic24_chip.h  \
+../../../../../../ece3724/pic24_code_examples/lib/include/pic24_libconfig.h  \
+../../../../../../ece3724/pic24_code_examples/lib/include/pic24_uart.h  \
+../../../../../../ece3724/pic24_code_examples/lib/include/pic24_util.h  \
+../../../../../../ece3724/pic24_code_examples/lib/include/pic24_unittest.h 
