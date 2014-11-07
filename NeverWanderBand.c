5e689d4bfa6c620ad484ce87aa71c76d2e9e8aa2
cekirk01 <cek72@msstate.edu>
2014-11-06 21:31:25 -0600

    Updated Pin Info

--- NeverWanderBand.c
+++ NeverWanderBand.c
@@ -11,13 +11,12 @@ int main(void) {
 
    //Defining Pins
   CONFIG_RB10_AS_DIG_INPUT();           // RX pin must be digital input
-  CONFIG_U1RX_TO_RP(RB10_RP);           // U1RX <- RB10
+  CONFIG_U1RX_TO_RP(RB12_RP);           // U1RX <- RB12
   CONFIG_RB11_AS_DIG_OUTPUT();           // TX pin must be digital output
   CONFIG_U1TX_TO_RP(RB11_RP);            // U1TX -> RB11
 
   printResetCause();       //print statement about what caused reset
   outString(HELLO_MSG);
-<<<<<<< HEAD
 
   char c_char;
   c_char = inChar2();
@@ -26,8 +25,4 @@ int main(void) {
     {c_char = inChar2();
         outChar1(c_char);
     };
-=======
-  char y[] = inChar2();
-  while(1){};
->>>>>>> parent of f871598... Added char receive loop
 }
